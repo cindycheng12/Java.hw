826946552202d80661da996f083fd1a677f4f0cc
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-09-21 11:50:32 +0800

    Add files via upload

---
+++ class05.java
@@ -0,0 +1,18 @@
+class CWin{
+    int width;
+    int height;
+    String name;
+    vooid setW(int w){
+
+    }
+    void setH(int h){
+
+    }
+    void setName(String s){
+        name=s;
+    }
+    public void show() {
+        System.out.println("Name="+name);
+        System.out.println("W="+width+",H="+height);
+    }
+}
